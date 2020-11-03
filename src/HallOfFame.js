3a849597aeebf7e90da62a293ad8c94438cdab59
efischer <efischer@student.42.fr>
2020-11-03 14:55:57 +0100

    add prop-types

--- src/HallOfFame.js
+++ src/HallOfFame.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './HallOfFame.css'
 
 const HallOfFame = ({ entries }) => (
   <table className="hallOfFame">
     <tbody>
-      <tr>FIXME</tr>
+      {
+        entries.map(({ date, guesses, id, player }) => (
+          <tr key={id}>
+            <td className="date">{date}</td>
+            <td className="guesses">{guesses}</td>
+            <td className="player">{player}</td>
+          </tr> 
+        ))
+      }
     </tbody>
   </table>
 )
+
+HallOfFame.propTypes = {
+  entries: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      guesses: PropTypes.number.isRequired,
+      id: PropTypes.number.isRequired,
+      player: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+}
 
 export default HallOfFame
 
